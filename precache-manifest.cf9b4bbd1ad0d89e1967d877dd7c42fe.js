792a0d34caaaa3308aeabdec6f94a65948d870cb
Danish Manhas <manhasd6@gmail.com>
2018-12-02 17:01:52 +0530

    Updates

---
+++ precache-manifest.cf9b4bbd1ad0d89e1967d877dd7c42fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf8237627a014de5f61a8e58bf8e4038",
+    "url": "/mycard/static/media/myImage.bf823762.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mycard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aac2154648500ed44688",
+    "url": "/mycard/static/js/runtime~main.aac21546.js"
+  },
+  {
+    "revision": "39c6183d4d2ce6371f36",
+    "url": "/mycard/static/js/main.39c6183d.chunk.js"
+  },
+  {
+    "revision": "c0a57c03a69e4ecd2e9c",
+    "url": "/mycard/static/js/1.c0a57c03.chunk.js"
+  },
+  {
+    "revision": "39c6183d4d2ce6371f36",
+    "url": "/mycard/static/css/main.83758c59.chunk.css"
+  },
+  {
+    "revision": "45ff2f88b175eea3df6bbd96680bfc5a",
+    "url": "/mycard/index.html"
+  }
+];
